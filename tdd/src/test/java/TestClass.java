11254cd80657d1d09476d27d595caa76c4e85425
Sunyoung Shin <s2017i30@e-mirim.hs.kr>
2019-03-02 16:06:18 +0900

    java tdd config

---
+++ tdd/src/test/java/TestClass.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class TestClass {
+
+
+    @Test
+    public void testX(){
+        assertEquals("test1","test1");
+    }
+}
